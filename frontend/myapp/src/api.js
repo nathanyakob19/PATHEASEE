41b10d9c0ef0bc7fc117bf66982e8238a9bfad1b
nathanyakob19 <nathanwaghchoure>
2026-02-05 09:35:29 +0530

    frontend12

--- frontend/myapp/src/api.js
+++ frontend/myapp/src/api.js
@@ -1,6 +1,7 @@
 // src/api.js
+export const API_URL = process.env.REACT_APP_API_URL ;
+//|| "http://localhost:5000";
 
-export const API_URL = "http://localhost:5000";
 
 /* ---------- GET ---------- */
 export async function apiGet(path) {
