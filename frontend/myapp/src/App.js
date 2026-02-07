5349dc6c574a0883b5a55707980a8337f793accc
nathanyakob19 <nathanwaghchoure>
2026-02-08 00:32:43 +0530

    g

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -45,11 +45,11 @@ function markImageFailed(url) {
 
 /* ---------- CITY CARDS ---------- */
 const CITIES = [
-  { name: "Mumbai", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Mumbai_03-2016_30_Gateway_of_India.jpg/640px-Mumbai_03-2016_30_Gateway_of_India.jpg" },
-  { name: "Pune", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Shaniwar_wada_pune.jpg/640px-Shaniwar_wada_pune.jpg" },
-  { name: "Delhi", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/t/t1/India_Gate_in_New_Delhi_03-2016.jpg/640px-India_Gate_in_New_Delhi_03-2016.jpg" },
-  { name: "Bengaluru", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Bangalore_Palace_3.jpg/640px-Bangalore_Palace_3.jpg" },
-  { name: "Chennai", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Chennai_Central.jpg/640px-Chennai_Central.jpg" },
+  { name: "Mumbai", logo: "https://img.icons8.com/ios-filled/50/gateway-of-india.png" },
+  { name: "Pune", logo: "https://cdn-icons-png.flaticon.com/512/16025/16025176.png" },
+  { name: "Delhi", logo: "https://img.icons8.com/external-icongeek26-outline-icongeek26/64/external-india-gate-india-icongeek26-outline-icongeek26.png" },
+  { name: "Bengaluru", logo: "https://static.thenounproject.com/png/2165510-200.png" },
+  { name: "Chennai", logo: "https://media.istockphoto.com/id/1462602084/vector/hindu-temple-vector-illustration-dravidian-architecture-tamil-nadu-india.jpg?s=612x612&w=0&k=20&c=MfH1a09nU5eVHfhkt75k8Ed2nf4RTrxmNtqcyfcPWxs=" },
 ];
 
 /* ---------- DISTANCE ---------- */
