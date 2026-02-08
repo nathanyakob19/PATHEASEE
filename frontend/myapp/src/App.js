6c9ecad60c8121c378cc072ca264bc89d6506789
nathanyakob19 <nathanwaghchoure>
2026-02-08 14:44:43 +0530

    l78

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchBar from "./SearchBar";
 import RatingsGraph from "./RatingsGraph";
