a9d89ea276e37d3b77f82085065bd5dbca4f0b59
nathanyakob19 <nathanwaghchoure>
2026-02-09 09:28:09 +0530

    mobile-view12

--- frontend/myapp/src/PillNav.js
+++ frontend/myapp/src/PillNav.js
@@ -121,15 +121,7 @@ const PillNav = ({
   return (
     <div className="pill-nav-container">
       <nav className={`pill-nav ${className}`} aria-label="Primary" style={cssVars}>
-        <div
-          className="pill-nav-mobile-bar mobile-only"
-          style={{
-            backgroundImage: `url(${process.env.PUBLIC_URL}/pathease-logo.png)`,
-            backgroundRepeat: "repeat",
-            backgroundSize: "32px 32px",
-            backgroundPosition: "0 0",
-          }}
-        >
+        <div className="pill-nav-mobile-bar mobile-only">
           <button
             className="mobile-menu-button mobile-menu-left"
             onClick={toggleMobileMenu}
